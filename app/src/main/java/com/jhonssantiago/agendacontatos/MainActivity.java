9d9a4afd3af4b7f9a2d4e6407cc18496eaf7ae87
Jhonatan Santiago <jhonssantiago@gmail.com>
2023-05-30 19:21:11 -0300

    Referenciando objetos

--- app/src/main/java/com/jhonssantiago/agendacontatos/MainActivity.java
+++ app/src/main/java/com/jhonssantiago/agendacontatos/MainActivity.java
@@ -3,12 +3,19 @@ package com.jhonssantiago.agendacontatos;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.ListView;
 
 public class MainActivity extends AppCompatActivity {
-
+    private ListView listaContatos;
+    private Button btnNewContact;
+    private AdapterListContact adapterListContact;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        listaContatos = findViewById(R.id.listView);
+        btnNewContact = findViewById(R.id.btnNewContact);
+
     }
 }
